5df531f91e3ceda1670d98e8f353e7a69d642724
Celso Tadaki Sinoka <103610912+C-Tadaki-S@users.noreply.github.com>
2022-11-24 10:53:30 -0300

    Immediate Generator agora comentado.

--- ImmediateGenerator/immediateG.v
+++ ImmediateGenerator/immediateG.v
@@ -1,16 +1,16 @@
 module immediateG(instruction, immediate);
-    input [31:0] instruction; /*Instrucao de 32 bits*/
-    output reg [11:0] immediate; /*Gera um immediate de 12 bits*/    
+    input [31:0] instruction;      /*Instrucao de 32 bits*/
+    output reg [11:0] immediate;   /*Gera um immediate de 12 bits*/    
     always @(*) begin
         
-    case (instruction[6:0])
-    //BEQ
+        case (instruction[6:0])    /*Observa os bits da OpCode e seleciona certos bits da instruction como Immediate*/
+    //BEQ -> Branch if Equal;
     7'b1100011:
         immediate[11:0] = {instruction[31], instruction[7], instruction[30:25], instruction[11:8]};
-    //LW
+    //LW -> Load Word: Carrega uma palavra da DM para o registrador;
     7'b0000011:
         immediate[11:0] = instruction[31:20];
-    //SW
+    //SW -> Storage Word: Pega uma palavra do registrador e manda para o Data memory;
     7'b0100011:
         immediate[11:0] = {instruction[31:25], instruction[11:7]};
     default:
