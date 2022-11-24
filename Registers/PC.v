79e79e102fec4942561ae0ee73de8dcd881f704b
Lucas Bertan <bertan1707@gmail.com>
2022-11-24 15:45:58 -0300

    Update PC inputs

--- Registers/PC.v
+++ Registers/PC.v
@@ -1,6 +1,6 @@
-module  PC(datain, dataout, contro, reset, clk);
+module  PC(datain, dataout, enable, reset, clk);
     input [31:0] datain;       /*dados de entrada*/
-    input clk, contro, reset;  /*sinais de controle*/
+    input clk, enable, reset;  /*sinais de controle*/
     output reg [31:0] dataout; /*dados de saida*/
     initial begin
         dataout = 32'd0;       /*inicializando a saida como zero*/
@@ -8,7 +8,7 @@ module  PC(datain, dataout, contro, reset, clk);
     always @(posedge clk) begin
         if(reset == 1'b1) dataout = 32'd0;       /*reset ligado na subida do clock: a saida eh zerada*/
         else begin
-            if(contro == 1'b1) dataout = datain; /*contro vale como enable, seta o valor da saida com a palavra de entrada*/
+            if(enable == 1'b1) dataout = datain; /*seta o valor da saida com a palavra de entrada*/
         end
     end
 endmodule
