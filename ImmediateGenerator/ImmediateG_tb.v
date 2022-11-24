f772d9e55e7073ab42d74f137836359c50d3b00c
Celso Tadaki Sinoka <103610912+C-Tadaki-S@users.noreply.github.com>
2022-11-24 11:03:26 -0300

    Testbench devidamente comentado
    Testbench do immediate generator foi comentado e nomeado devidamente.

--- ImmediateGenerator/ImmediateG_tb.v
+++ ImmediateGenerator/ImmediateG_tb.v
@@ -1,14 +1,15 @@
 `timescale 1ns / 100 ps
-module testimmG();
+
+module immediateG();
     reg [31:0]instruction_tb;
     wire [11:0] immediate_tb;
     immediateG uut (.instruction(instruction_tb),.immediate(immediate_tb));
 
-
     initial begin
+       
         //CASO DEFAULT 
         instruction_tb = 32'b11111111111111111111111110000000;
-        #20
+        #20 /*Intervalo setado para garantir que os bits foram comunicados*/
         $display("CASO DEFAULT");
         $display("Immediate = %b\n", immediate_tb);
         if(immediate_tb == 12'b000000000000)
@@ -20,32 +21,31 @@ module testimmG();
         //BEQ
         instruction_tb = 32'b00001111111111111111111101100011;
         #20
-        $display ("CASO BEQ");
+        $display ("\nCASO BEQ");
         $display("Immediate = %b\n", immediate_tb);
         if(immediate_tb == 12'b000001111111)
-            $display("Ta OK\n");
+            $display("OK\n");
         else
-            $display("Nao Ta OK");
-
+            $display("Erro");
         //LW
         instruction_tb = 32'b01010101010101111111111110000011;
         #20
-        $display("CASO LW");
+        $display("\nCASO LW");
         $display("Immediate = %b\n", immediate_tb);
         if(immediate_tb == 12'b010101010101)
-            $display("Ta OK\n");
+            $display("OK\n");
         else
-            $display("Nao Ta OK");
+            $display("Erro");
 
         //SW
         instruction_tb = 32'01010101111111111111101010100011;
         #20
-        $display("CASO SW");
+        $display("\nCASO SW");
         $display("Immediate = %b\n", immediate_tb);
         if(immediate_tb == 12'b010101010101)
-            $display("Ta OK\n");
+            $display("OK\n");
         else
-            $display("Nao Ta OK");
+            $display("Erro");
         $stop; 
     end
 endmodule
