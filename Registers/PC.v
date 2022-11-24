62fd810782ba2a34dd79fd5ddfbc1ada0ffa7418
Celso Tadaki Sinoka <103610912+C-Tadaki-S@users.noreply.github.com>
2022-11-24 11:39:09 -0300

    PC comentado e corrigido
    "posedge reset" retirada da condição de leitura do "always"

--- Registers/PC.v
+++ Registers/PC.v
@@ -1,14 +1,14 @@
 module  PC(datain, dataout, contro, reset, clk);
-    input [31:0] datain;
-    input clk, contro, reset;
-    output reg [31:0] dataout;
+    input [31:0] datain;       /*dados de entrada*/
+    input clk, contro, reset;  /*sinais de controle*/
+    output reg [31:0] dataout; /*dados de saida*/
     initial begin
-        dataout = 32'd0;
+        dataout = 32'd0;       /*inicializando a saida como zero*/
     end
-    always @(posedge clk, posedge reset) begin
-        if(reset == 1'b1) dataout = 32'd0;
+    always @(posedge clk) begin
+        if(reset == 1'b1) dataout = 32'd0;       /*reset ligado na subida do clock: a saida eh zerada*/
         else begin
-            if(contro == 1'b1) dataout = datain;
+            if(contro == 1'b1) dataout = datain; /*contro vale como enable, seta o valor da saida com a palavra de entrada*/
         end
     end
 endmodule
